a6f978e825bca084acd560574a8ee0c5439858b6
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-08-28 02:43:17 +0300

    feat: connect products API

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,5 +1,7 @@
+import { PRODUCTS_API_URL } from '../config';
+
 export const API_PATHS = {
-	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	products: PRODUCTS_API_URL,
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
